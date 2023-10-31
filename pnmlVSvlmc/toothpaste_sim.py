ca99950ea3dc64a7ae0bb6fa03a04a99b334d933
emilio-imt <emilio-imt@MacBook-Pro.local>
2023-10-31 18:38:44 +0100

    parallel

--- pnmlVSvlmc/toothpaste_sim.py
+++ pnmlVSvlmc/toothpaste_sim.py
@@ -4,7 +4,9 @@ Simple Python wrapper for the toothpaste miner Haskell implementation.
 
 import os.path
 import subprocess
+import multiprocessing as mp
 import sys
+import time
 
 import pm4py
 from pm4py.objects.log.importer.xes import importer as xes_importer
@@ -37,7 +39,7 @@ logfile.
 For richer parameters, such as noise reduction, see the command line help
 for Toothpaste miner.
 """
-def mine(logfile,outfile=None):
+def mine(logfile,outfile=None,noise="0"):
     ofile = outfile
     if ofile is None:
         bname =  logfile.name.split(".")[0]
@@ -57,6 +59,7 @@ def mine(logfile,outfile=None):
     subprocess.run([TBIN,
                     "--logformat=dcdt",
                     "--eventlog", dfile,
+                    "--noise=%s"%(noise),
                     "--pnetfile", pnfile,
                     "--ptreefile", ptfile] )
     pn,im, fm = pm4py.read_pnml(pnfile)
@@ -98,20 +101,27 @@ def getCliArgs():
     else:
         outfile=None
 
-def learnSPN():
-    (pn,pnfile) = mine(logfile,outfile)
-    pm4py.view_petri_net(pn)
+def learnSPN(noise="0"):
+    (pn,pnfile) = mine(logfile,outfile,noise)
+    #pm4py.view_petri_net(pn)
 
-def simSPN(pnfile, nrun=1):
-    pn,im, fm = pm4py.read_pnml(pnfile)
+def simSPN(pnfile=None,queue=None,nrun=1):
+    pn=None
+    if(type(pnfile)==str):
+        pn,im, fm = pm4py.read_pnml(pnfile)
+    else:
+        pn=pnfile
+
     im = pm4py.generate_marking(pn, {'pI': 1})
-
     smap=tpsmap.get_stoch_map(pn,str(pnfile))
 
     simulated_log = simulator.apply(pn, im,variant=simulator.Variants.STOCHASTIC_PLAYOUT,
         parameters={simulator.Variants.STOCHASTIC_PLAYOUT.value.Parameters.NO_TRACES: nrun,
                     simulator.Variants.STOCHASTIC_PLAYOUT.value.Parameters.STOCHASTIC_MAP:smap  })
-    return simulated_log
+    if(queue is not None):
+        queue.put(simulated_log[0])
+    else:
+        return simulated_log
 
 def saveSimLog(log=None,outlogFile=None):
     print(outlogFile)
@@ -128,13 +138,11 @@ def saveSimLog(log=None,outlogFile=None):
                     trace+=",,"+t1["concept:name"]
         outtraces.write(trace+"\n")
     outtraces.close()
-
-
     
 
 if __name__ == "__main__":
     getCliArgs()
-    #learnSPN()
+    #learnSPN(noise="0")
 
     pnfile=None
     if(outfile is None):
@@ -142,13 +150,26 @@ if __name__ == "__main__":
     else:
         pnfile=outfile
 
-    logs=[]
-    nrun=int(10000)
-    step=int(100)
-    for i in tqdm(range(0,nrun,step)):
-        logs.extend(simSPN(pnfile=str(pnfile),nrun=step))
+    mp.set_start_method('spawn')
+    manager = mp.Manager()
+    logqueue = manager.Queue()
+    # create a default process pool
+    pool = mp.Pool()
+    st=time.time()
+    pool.starmap(simSPN, [(str(pnfile),logqueue,1) for i in range(3)])
+    print(time.time()-st)
+    pool.close()
+    pool.join()
 
     outlogFile=pathlib.Path("%s/%s_sim.dcdt"%(pnfile.parent,pnfile.name.split(".")[0]))
-    saveSimLog(logs,outlogFile)
+    results = []
+    while not logqueue.empty():
+        results.append(logqueue.get())
+
+    saveSimLog(results,outlogFile)
+
+
+    manager.shutdown()
+    
 
 
